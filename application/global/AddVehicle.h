7298181c5d1be3ad4a5d0ee0cc7232fc9f237f0b
Mani Amoozadeh <maniam@ucdavis.edu>
2015-06-18 23:37:36 -0700

    adding new files to the global folder
    Former-commit-id: 6a548afc4c3388c2e1fd796b679dbdaca3fb0c69
    Former-commit-id: 8e101f313f496ca14312ee00d7bc218f586d513a

---
+++ application/global/AddVehicle.h
@@ -0,0 +1,77 @@
+/****************************************************************************/
+/// @file    AddVehicle.h
+/// @author  Mani Amoozadeh <maniam@ucdavis.edu>
+/// @author  second author name
+/// @date    August 2013
+///
+/****************************************************************************/
+// VENTOS, Vehicular Network Open Simulator; see http:?
+// Copyright (C) 2013-2015
+/****************************************************************************/
+//
+// This file is part of VENTOS.
+// VENTOS is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef VEHICLEADD
+#define VEHICLEADD
+
+#include "BaseModule.h"
+#include "TraCI_Extend.h"
+
+#define SSTR(x) dynamic_cast< std::ostringstream & >( (std::ostringstream() << std::dec << x ) ).str()
+
+namespace VENTOS {
+
+class AddVehicle : public BaseModule
+{
+	public:
+		virtual ~AddVehicle();
+		virtual void initialize(int stage);
+        virtual void handleMessage(cMessage *msg);
+		virtual void finish();
+	    virtual void receiveSignal(cComponent *, simsignal_t, long);
+
+	private:
+
+        // NED variables
+        cModule *nodePtr;   // pointer to the Node
+        TraCI_Extend *TraCI;  // pointer to the TraCI module
+        double terminate;
+        simsignal_t Signal_executeFirstTS;
+        bool on;
+        int mode;
+	    int totalVehicles;
+	    double lambda;
+	    int plnSize;
+	    double plnSpace;
+
+	    // methods
+        void Add();
+
+	    void Scenario1();
+        void Scenario2();
+        void Scenario3();
+        void Scenario4();
+        void Scenario5();
+        void Scenario6();
+        void Scenario7();
+        void Scenario8();
+        void Scenario9();
+};
+
+}
+
+#endif
---
+++ application/global/VehicleSpeedProfile.h
@@ -0,0 +1,78 @@
+/****************************************************************************/
+/// @file    VehicleSpeedProfile.h
+/// @author  Mani Amoozadeh <maniam@ucdavis.edu>
+/// @author  second author name
+/// @date    August 2013
+///
+/****************************************************************************/
+// VENTOS, Vehicular Network Open Simulator; see http:?
+// Copyright (C) 2013-2015
+/****************************************************************************/
+//
+// This file is part of VENTOS.
+// VENTOS is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef SPEEDPROFILE
+#define SPEEDPROFILE
+
+#include "BaseModule.h"
+#include "TraCI_Extend.h"
+
+namespace VENTOS {
+
+class SpeedProfile : public BaseModule
+{
+	public:
+		virtual ~SpeedProfile();
+		virtual void initialize(int stage);
+        virtual void handleMessage(cMessage *msg);
+		virtual void finish();
+
+	public:
+        void Change();
+
+	private:
+
+        // NED variables
+        cModule *nodePtr;   // pointer to the Node
+        TraCI_Extend *TraCI;
+        bool on;
+	    int mode;
+	    std::string laneId;
+	    double minSpeed;
+        double normalSpeed;
+        double maxSpeed;
+        double switchTime;
+        double startTime;  // the time that speed profiling starts
+        std::string trajectoryPath;
+
+	    // class variables
+        std::string profileVehicle;
+        std::string lastProfileVehicle;
+	    double old_speed;
+	    double old_time;
+        FILE *f2;
+        bool endOfFile;
+
+	    // method
+	    void AccelDecel(double, double, double);
+	    void AccelDecelZikZak(double, double, double);
+        void AccelDecelPeriodic(double, double, double, double);
+	    void ExTrajectory(double);
+};
+}
+
+#endif
