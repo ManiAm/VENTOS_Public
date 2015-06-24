bb920c763a090456f2a409284d579caa60f6fce1
Mani Amoozadeh <maniam@ucdavis.edu>
2015-06-24 13:26:03 -0700

    Intersection delay script now can plot the speed profile nicely + adding uiTable. --now working on delay measurements per phase
    Former-commit-id: 1778e98f050114cd999f1c6c35ebaeed27d097d8
    Former-commit-id: 82d6526a2854c1173fb943bad1b4795a66c4b3d6

--- matlab/IntersectionDelay.m
+++ matlab/IntersectionDelay.m
@@ -5,11 +5,14 @@ clc;    % position the cursor at the top of the screen
 %clf;   % closes the figure window
 
 % ---------------------------------------------------------------
-folderPath = '../results/cmd/R&B';
+
+disp('reading file ...');
+
+folderPath = '../results/cmd/test3_noRightTurns';
 
 path = strcat(folderPath,'/0_vehicleData.txt');    
 file_id = fopen(path);
-formatSpec = '%d %f %s %s %s %f %f %f %s %f %f %f %s %d';
+formatSpec = '%d %f %s %s %s %f %f %f %s %f %f %f %s';
 C_text = textscan(file_id, formatSpec, 'HeaderLines', 3);
 fclose(file_id);
 
@@ -18,23 +21,25 @@ timeSteps = C_text{1,2};
 vehicles = C_text{1,3}; 
 lanes = C_text{1,5};
 speeds = C_text{1,7};
-signal = C_text{1,14};
-
-disp('reading file is done!');
+signal = C_text{1,13};
 
 % ---------------------------------------------------------------
+
+disp('parsing file ...');
     
 % stores vehicle IDs
-vIDs = {};
+vIDs = unique(vehicles);
+vIDs = sort_nat(vIDs);
+VehNumbers = size(vIDs,1);
 
 [rows,~] = size(vehicles);
 n = indices(end,1);
 
 % preallocating and initialization with -1
 vehiclesTS = zeros(n,1) - 1;
-vehiclesSpeed = zeros(n,1) - 1;
-vehiclesLane = cell(n,1);
-vehicleSignal = zeros(n,1) - 1;
+vehiclesSpeed = zeros(n,VehNumbers) - 1;
+vehiclesLane = cell(n,VehNumbers);
+vehicleSignal = cell(n,VehNumbers);
 
 for i=1:rows   
        
@@ -46,24 +51,20 @@ for i=1:rows
     % get the current vehicle name
     vehicle = char(vehicles(i,1));
         
-    vNumber = find(ismember(vIDs,vehicle));        
-    if( isempty(vNumber) )
-        vIDs{end+1} = vehicle;
-        [~,vNumber] = size(vIDs);
-    end   
+    vNumber = find(ismember(vIDs,vehicle)); 
 
     vehiclesSpeed(index,vNumber) = double(speeds(i,1));         
     vehiclesLane(index, vNumber) = lanes(i,1);
-    vehicleSignal(index, vNumber) = int32(signal(i,1));
+    vehicleSignal(index, vNumber) = signal(i,1);
 end
 
-disp('parsing file is done!');
-clearvars C_text indices timeSteps lanes speeds signal
+clear C_text indices timeSteps lanes speeds signal
     
-[~,VehNumbers] = size(vIDs);
 [rows,~] = size(vehiclesLane);
 
 % ---------------------------------------------------------------
+
+disp('calculation ...');
 
 % vehiclesLane contains the lanes (controlled by a TL) that a vehicle traveres 
 % before reaching to the intersection. We are only interested into the last lane
@@ -200,194 +201,172 @@ end
 
 % -----------------------------------------------------------------
 
-% TODO: I should wait for the implementation of the TraCI command that
-% gives the TL status ahead!
+%{
+At this point, we have the starting point of all delay components:
+- index(1,i): start of the deceleration delay
+- index(2,i): start of the stopping delay
+- index(3,i): start of the acceleration delay
+- index(4,i): end of the acceleration delay
+%}
 
-
-% at this point, we have the starting point of all delay components:
-% index(1,i): start of the deceleration delay
-% index(2,i): start of the stopping delay
-% index(3,i): start of the acceleration delay
-% index(4,i): end of the acceleration delay
-
-% the slowing down can be due to many reasons other than TL.
-% we are only interested into those slowing downs that are due to red or
-% yellow signal
-
-% for i=1:VehNumbers  
-%     if( index(1,i) ~= -1)
-%         target = index(2,i);
-%         if(target == -1)
-%             target = index(3,i);
-%         end
-%         % index(1,i) and target specify the acceleration delay interval        
-%         newStart = index(1,i);
-%         for j=index(1,i):target
-%             if( vehicleSignal(j,i) ~= 1 )  % if the signal is not yellow or red
-%                 newStart = newStart + 1;
-%             end
-%         end
-%         
-%         if(newStart-1 == target)
-%             index(1,i) = -1;
-%             index(2,i) = -1;
-%             index(3,i) = -1;
-%             index(4,i) = -1;
-%         else
-%             index(1,i) = newStart;            
-%         end        
-%     end
-% end        
-         
-% -----------------------------------------------------------------
-
-% speed profile of vehicles before passing the intersection
-
-figNum = 0;
-
-if(false)
-
-    figure('units','normalized','outerposition',[0 0 1 1]);
-    
-    counter = 1;
-
-    for i=1:VehNumbers
-
-        figNum = floor( (i-1)/12 ) + 1;
-        hf(1) = figure(figNum);
-        set(gcf,'name','Speed');
-
-        subplot(4,3,counter);    
-        handle1 = plot(vehiclesTS(:,1),vehiclesSpeed(:,i),'LineWidth', 3);
-
-        counter = counter + 1;
-        % reset the counter
-        if(counter == 13)
-            counter = 1;
-        end
-    
-        % set the x-axis limit
-        set( gca, 'XLim', [0 vehiclesTS(end)] );
-    
-        % set the y-axis limit
-        set( gca, 'YLim', [-2 34] );
-
-        % set font size
-        set(gca, 'FontSize', 17);
-
-        grid on;
-    
-        title( char(vIDs{i}) );
-    
-        % if the vehicle did not cross the intersection at all
-        if( crossed(i) == 0 )        
-            text(4, 13, 'Not crossed!');
-        end    
-    
-        hold on;
-    
-        % mark deceleration delay
-        if( index(1,i) ~= -1 )
-            endPoint = index(2,i);
-            if(endPoint == -1)        
-                endPoint = index(3,i);
-            end        
-            X = vehiclesTS(index(1,i):endPoint, 1);
-            Y = vehiclesSpeed(index(1,i):endPoint, i);
-            plot(X, Y, 'LineWidth', 3, 'Color','red');
-        end
-    
-        % mark stopping delay
-        if( index(2,i) ~= -1 && index(3,i) ~= -1 )
-            X = vehiclesTS(index(2,i):index(3,i), 1);
-            Y = vehiclesSpeed(index(2,i):index(3,i), i);
-            plot(X, Y, 'LineWidth', 3, 'Color','black');
-        end
-    
-        % mark acceleration delay
-        if( index(3,i) ~= -1 && index(4,i) ~= -1 )
-            X = vehiclesTS(index(3,i):index(4,i), 1);
-            Y = vehiclesSpeed(index(3,i):index(4,i), i);
-            plot(X, Y, 'LineWidth', 3, 'Color','green');
-        end
-    
-        hold off;    
-    
-%     % mark slowing down and waiting time with an arrow
-%     if( index(1,i) ~= -1 && index(2,i) ~= -1 )
-%         startC = [vehiclesTS(index(1,i),1), vehiclesSpeed(index(1,i),i)]; 
-%         endC = [vehiclesTS(index(2,i),1), vehiclesSpeed(index(1,i),i)];
-%         arrow(startC, endC, 10, 'BaseAngle', 20, 'Ends', 3);
-%     end
-%     
-%     % mark speeding up with an arrow
-%     if( indexSpeedUp(1,i) ~= -1 && indexSpeedUp(2,i) ~= -1 )
-%         startC = [vehiclesTS(indexSpeedUp(1,i),1), vehiclesSpeed(indexSpeedUp(1,i),i)]; 
-%         endC = [vehiclesTS(indexSpeedUp(2,i),1), vehiclesSpeed(indexSpeedUp(1,i),i)];
-%         arrow(startC, endC, 10, 'BaseAngle', 20, 'Ends', 3);
-%     end
-
-    end
-end
-
-% -----------------------------------------------------------------
-
-% we need to get TL phasing information
+% we need to get TL phasing information + queue size
 
 path = strcat(folderPath, '/0_intersectionData.txt');    
 file_id = fopen(path);
-formatSpec = '%d %f %s %s %d';
+formatSpec = '%s %d %s %f %f %f %f %d %d';
 C_text = textscan(file_id, formatSpec, 'HeaderLines', 2);
 fclose(file_id);
 
-phaseNumbers = C_text{1,1};
-endTime = C_text{1,2};
-queueSize = C_text{1,5};
+phaseNumbers = C_text{1,2};
+greenStart = C_text{1,4};
+yellowStart = C_text{1,5};
+redStart = C_text{1,6};
+endTime = C_text{1,7};
+lanesCount = C_text{1,8};
+queueSize = C_text{1,9};
 
-% store start/end time of each phase in phaseDuration
+totalPhases = size(phaseNumbers,1);
+phaseDuration = zeros(4,totalPhases);
+phaseData = zeros(2,totalPhases);
 
-[rows,~] = size(phaseNumbers);
-oldIndex = -1;
-oldTime = 0;
-
-for i=1:rows 
-    phaseNumber = int32(phaseNumbers(i,1));
-    time = double(endTime(i,1));
+for i=1:totalPhases
+    phaseDuration(1,i) = double(greenStart(i,1));
+    phaseDuration(2,i) = double(yellowStart(i,1)); 
+    phaseDuration(3,i) = double(redStart(i,1));
+    phaseDuration(4,i) = double(endTime(i,1));
     
-    if(oldIndex ~= phaseNumber)
-        phaseDuration(1,phaseNumber) = oldTime;
-        phaseDuration(2,phaseNumber) = time;
-        
-        oldIndex = phaseNumber;
-        oldTime = time;
-    end   
+    % store average queue size
+    if( double(queueSize(i,1)) ~= -1)
+        phaseData(1,i) = double(queueSize(i,1)) / double(lanesCount(i,1));
+    else
+        phaseData(1,i) = -1;
+    end
 end
 
 % -----------------------------------------------------------------
 
-% store average queue size at the end of each phase in phaseData(1,:)
+% draw speed profile of vehicles and mark delay components with colors
+% as well as showing TL states for each movement
 
-oldPhaseNumber = 1;
-totalQSize = 0;
-numLanes = 0;
+figLimit = 2;    % limit the number of windows shown (-1: show all)
+counter = 1;
+figNum = 0; 
+figNum_old = -1;
 
-for i=1:rows 
-    phaseNumber = int32(phaseNumbers(i,1));
+for i=1:VehNumbers
+
+    figNum = floor( (i-1)/8 ) + 1;
     
-    if(oldPhaseNumber ~= phaseNumber)        
-        phaseData(1,phaseNumber-1) = double(totalQSize) / numLanes;
-        oldPhaseNumber = phaseNumber;
-        totalQSize = 0;
-        numLanes = 0;
+    % show only one figure
+    if(figLimit ~= -1 && figNum > figLimit)
+        break;    
+    elseif(figNum ~= figNum_old)
+        figure('name','Speed', 'units', 'normalized', 'outerposition', [0 0 1 1], 'outerposition', [0 0 1 1]);
+        figNum_old = figNum;
+    end
+
+    subplot(4,2,counter); 
+    
+    % get rid of the negative speed
+    X = vehiclesTS;
+    Y = vehiclesSpeed(:,i);    
+    t = (Y < 0);    
+    X(t) = [];
+    Y(t) = [];    
+    
+    plot(X, Y,'LineWidth', 3);
+
+    counter = counter + 1;
+    % reset the counter
+    if(counter == 9)
+        counter = 1;
     end
     
-    qSize = int32(queueSize(i,1));
-    totalQSize = totalQSize + qSize;
-    numLanes = numLanes + 1; 
+    % set the x-axis limit
+    set( gca, 'XLim', [0 vehiclesTS(end)] );
     
-    if(i == rows)
-        phaseData(1,phaseNumber) = double(totalQSize) / numLanes;
+    % set the y-axis limit
+    set( gca, 'YLim', [-2 36] );
+
+    % set font size
+    set(gca, 'FontSize', 17);
+
+    grid on;
+    
+    title( char(vIDs{i}) );
+    
+    % if the vehicle did not cross the intersection at all
+    if( crossed(i) == 0 )        
+        text(4, 13, 'Not crossed!');
+    end  
+    
+    hold on;
+    
+    % mark deceleration delay with red
+    if( index(1,i) ~= -1 )
+        endPoint = index(2,i);
+        if(endPoint == -1)        
+            endPoint = index(3,i);
+        end        
+        X = vehiclesTS(index(1,i):endPoint, 1);
+        Y = vehiclesSpeed(index(1,i):endPoint, i);
+        plot(X, Y, 'LineWidth', 3, 'Color','red');
     end
+    
+    % mark stopping delay with black
+    if( index(2,i) ~= -1 && index(3,i) ~= -1 )
+        X = vehiclesTS(index(2,i):index(3,i), 1);
+        Y = vehiclesSpeed(index(2,i):index(3,i), i);
+        plot(X, Y, 'LineWidth', 3, 'Color','black');
+    end
+    
+    % mark acceleration delay with green
+    if( index(3,i) ~= -1 && index(4,i) ~= -1 )
+        X = vehiclesTS(index(3,i):index(4,i), 1);
+        Y = vehiclesSpeed(index(3,i):index(4,i), i);
+        plot(X, Y, 'LineWidth', 3, 'Color','green');
+    end
+    
+    % show TL status ahead for this vehicle
+    
+    % TODO: currently it is 0 or 1. I should change it to g G y r later.
+    % I should wait for the implementation of the TraCI command that
+    % gives the TL status ahead!
+    for k=1:size(vehicleSignal,1)    
+        signalStatus = vehicleSignal(k,i);
+        
+        if(strcmp(signalStatus,'g') == 1)
+            line([vehiclesTS(k) vehiclesTS(k)+0.09],[32 32], 'LineWidth', 3, 'Color', rgb('LightGreen'));            
+        elseif(strcmp(signalStatus,'G') == 1)
+            line([vehiclesTS(k) vehiclesTS(k)+0.09],[32 32], 'LineWidth', 3, 'Color', rgb('DarkGreen'));     
+        elseif(strcmp(signalStatus,'y') == 1)
+            line([vehiclesTS(k) vehiclesTS(k)+0.09],[32 32], 'LineWidth', 3, 'Color', 'y'); 
+        elseif(strcmp(signalStatus,'r') == 1)
+            line([vehiclesTS(k) vehiclesTS(k)+0.09],[32 32], 'LineWidth', 3, 'Color', 'r'); 
+        elseif(strcmp(signalStatus,'n') == 1)
+            line([vehiclesTS(k) vehiclesTS(k)+0.09],[32 32], 'LineWidth', 3, 'Color', 'k'); 
+        end       
+    end
+    
+    % show phase numbering for this vehicle
+    phasesCount = size(phaseDuration,2);
+    for p=1:phasesCount        
+        if(phaseDuration(1,p) ~= -1 && phaseDuration(4,p) ~= -1)
+            Start = [phaseDuration(1,p) 34];
+            Stop = [phaseDuration(4,p) 34];
+            arrow(Start, Stop, 'Ends', 3); 
+        end
+        
+        % if the finish time of the last phase is -1
+        if(p == phasesCount && phaseDuration(1,p) ~= -1 && phaseDuration(4,p) == -1)
+            Start = [phaseDuration(1,p) 34];
+            Stop = [vehiclesTS(end) 34];
+            arrow(Start, Stop, 'Ends', 2);             
+        end
+    end
+    
+    hold off;
+    
 end
 
 % -----------------------------------------------------------------
@@ -418,12 +397,103 @@ end
 
 % -----------------------------------------------------------------
 
+% making a cell array that contains the complete view of the system
+
+if(false)
+    
+    cell1 = vIDs;
+    cell2 = num2cell(crossed');
+    cell3 = [ num2cell(index(1,:)') num2cell(index(2,:)') num2cell(index(3,:)') num2cell(index(4,:)') ];
+    cell4 = [ num2cell(indexTS(1,:)') num2cell(indexTS(2,:)') num2cell(indexTS(3,:)') num2cell(indexTS(4,:)') ];
+
+    data = [ cell1 cell2 cell3 cell4 ]';
+
+    f = figure();
+
+    t = uitable('Parent', f, 'Units', 'normalized', 'Position', [0.08 0.6 0.85 0.3], 'Data', data );
+    t.Data = data;
+    t.RowName = {'Veh ID', 'crossed?', 'deceleration start index', 'stopping start index', 'acceleration start index', 'acceleration end index', 'deceleration start TS', 'stopping start TS', 'acceleration start TS', 'acceleration end TS'}; 
+
+end
+
+% -----------------------------------------------------------------
+
+%{ 
+
+if(vehicle does not slow down before the intersection)
+{
+    // it is getting green and has right of way
+    // this happens when a movement gets G
+    // the delay is zero for this vehicle.
+}
+else if(vehicle slows down)
+{
+    if(isGreen and g)
+    {
+        // vehicle slows down and the TL ahead is green.
+
+        // for example it is slowing down before turning right or
+        // a preceeding vehicle is blocking its way.
+        // the delay is zero for this vehicle.
+    }
+    else
+    {
+        //
+
+    }
+}
+
+
+%}
+
+
+i = 0;
+
+
+
+
+
+
+% the slowing down can be due to many reasons other than red/yellow TL.
+% for example the vehicle slows down when turning at intersection
+% we are only interested into those slowing downs that are due to red or
+% yellow signal
+
+% for i=1:VehNumbers  
+%     if( index(1,i) ~= -1)
+%         target = index(2,i);
+%         if(target == -1)
+%             target = index(3,i);
+%         end
+%         % index(1,i) and target specify the acceleration delay interval        
+%         newStart = index(1,i);
+%         for j=index(1,i):target
+%             if( vehicleSignal(j,i) ~= 1 )  % if the signal is not yellow or red
+%                 newStart = newStart + 1;
+%             end
+%         end
+%         
+%         if(newStart-1 == target)
+%             index(1,i) = -1;
+%             index(2,i) = -1;
+%             index(3,i) = -1;
+%             index(4,i) = -1;
+%         else
+%             index(1,i) = newStart;            
+%         end        
+%     end
+% end        
+         
+
+
 % calculate the average delay at the end of each phase
 
 for j=1:size(phaseDuration,2)  % j is phase number
    
-   totalDelay = 0;  % total delay in this phase
-   vehCount = 0;  % total vehicles in this phase
+   crossedVehCount = 0;   % number of crossed vehicles in this phase
+   delayedVehCount = 0;   % number of delayed vehicles
+   vehCount = 0;
+   totalDelay = 0;
    phaseStart = phaseDuration(1,j);
    phaseEnd = phaseDuration(2,j);
    
@@ -434,22 +504,19 @@ for j=1:size(phaseDuration,2)  % j is phase number
            difference = d2 - d1;
            if(difference > 0)
                totalDelay = totalDelay + difference;
-               vehCount = vehCount + 1;
+               delayedVehCount = delayedVehCount + 1;
            end
        end
    end
    
    phaseData(2,j) = totalDelay/vehCount;
-   %fprintf( 'phase %d: totalDelay=%0.2f, vehCount=%d, aveDelay=%0.2f\n', j, totalDelay, vehCount, totalDelay/vehCount);
+   fprintf( 'phase %d: crossedVehCount=%d, delayedVehCount=%d, totalDelay=%0.2f, aveDelay=%0.2f\n', j, 0, delayedVehCount, totalDelay, totalDelay/vehCount);
     
 end
 
 % -----------------------------------------------------------------
 
 figure('units','normalized','outerposition',[0 0 1 1]);
-
-figNum = figNum + 1;
-hf(1) = figure(figNum);
 set(gcf,'name','Speed');
 
 subplot(2,1,1);
