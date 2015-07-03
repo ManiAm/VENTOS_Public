44ef6dc2bc6c5fe819a6847c42006a906a8e3183
Mani Amoozadeh <maniam@ucdavis.edu>
2015-07-02 17:57:51 -0800

    collecting intersection delay for each vehicle directly in the simulation. This makes the matlab script smaller and much much faster!
    Former-commit-id: 8b5ccddb0618c5665ff4a617c8384ee8b268f779
    Former-commit-id: 3c10ccefd1f0e11ba45e63d381f226255a6d3a8f

--- application/trafficLight/03_VehDelay.cc
+++ application/trafficLight/03_VehDelay.cc
@@ -93,7 +93,29 @@ void VehDelay::vehiclesDelay()
         std::list<std::string> allVeh = TraCI->laneGetLastStepVehicleIDs( i->c_str() );
 
         for(std::list<std::string>::reverse_iterator k = allVeh.rbegin(); k != allVeh.rend(); ++k)
-            vehiclesDelayEach(k->c_str());
+        {
+            std::string vID = k->c_str();
+
+            // look for the vehicle in delay map
+            std::map<std::string, delayEntry>::iterator loc = intersectionDelay.find(vID);
+
+            // insert if new
+            if(loc == intersectionDelay.end())
+            {
+                boost::circular_buffer<std::pair<double,double>> CB_speed;  // create a circular buffer
+                CB_speed.set_capacity(10);                                  // set max capacity
+                CB_speed.clear();
+
+                boost::circular_buffer<std::pair<double,double>> CB_accel;  // create a circular buffer
+                CB_accel.set_capacity(10);                                  // set max capacity
+                CB_accel.clear();
+
+                delayEntry *entry = new delayEntry("", "", -1, false, -1, -1, -1, -1, -1, -1, CB_speed, CB_accel);
+                intersectionDelay.insert( std::make_pair(vID, *entry) );
+            }
+
+            vehiclesDelayEach(vID);
+        }
     }
 }
 
@@ -101,14 +123,197 @@ void VehDelay::vehiclesDelay()
 void VehDelay::vehiclesDelayEach(std::string vID)
 {
     // look for the vehicle in delay map
-    std::map<std::string, delayEntry>::iterator loc = delay.find(vID);
+    std::map<std::string, delayEntry>::iterator loc = intersectionDelay.find(vID);
+    delayEntry veh = loc->second;
 
+    // if the vehicle is controlled by a TL
+    if(veh.TLid == "")
+    {
+        // get the TLid that controls this vehicle
+        // empty string means the vehicle is not controlled by any TLid
+        std::string result = TraCI->vehicleGetTLID(vID);
+
+        if(result == "")
+            return;
+        else
+            loc->second.TLid = result;
+    }
+
+    // if the veh is on its last lane before the intersection
+    if(veh.lastLane == "")
+    {
+        // get current lane
+        std::string currentLane = TraCI->vehicleGetLaneID(vID);
+
+        // get best lanes for this vehicle
+        std::map<int,bestLanesEntry> best = TraCI->vehicleGetBestLanes(vID);
+
+        bool found = false;
+        for(std::map<int,bestLanesEntry>::iterator y = best.begin(); y != best.end(); ++y)
+        {
+            std::string laneID = y->second.laneId;
+            int offset = y->second.offset;
+            int continuingDrive = y->second.continuingDrive;
+
+            if(offset == 0 && continuingDrive == 1 && laneID == currentLane)
+            {
+                loc->second.lastLane = laneID;
+                loc->second.intersectionEntrance = simTime().dbl();
+                found = true;
+                break;
+            }
+        }
+
+        if(!found)
+            return;
+    }
+
+    // check if we have crossed the intersection
+    bool crossed = veh.crossedIntersection;
+    if(!crossed)
+    {
+        // if passed the intersection
+        if(TraCI->vehicleGetTLLinkStatus(vID) == 'n')
+        {
+            loc->second.crossedIntersection = true;
+            loc->second.crossedTime = simTime().dbl();
+            crossed = true;
+        }
+    }
+
+    // looking for the start of deceleration delay
+    double startDeccel = veh.startDeccel;
+    if(startDeccel == -1 && !crossed)
+    {
+        boost::circular_buffer<std::pair<double,double>> CB_speed = veh.lastSpeeds;
+        double speed = TraCI->vehicleGetSpeed(vID);
+        loc->second.lastSpeeds.push_back( std::make_pair(simTime().dbl(), speed) );
+
+        boost::circular_buffer<std::pair<double,double>> CB_accel = veh.lastAccels;
+        double accel = TraCI->vehicleGetCurrentAccel(vID);
+        loc->second.lastAccels.push_back( std::make_pair(simTime().dbl(), accel) );
+
+        if(CB_accel.full())
+        {
+            for(boost::circular_buffer<std::pair<double,double>>::iterator g = CB_accel.begin(); g != CB_accel.end(); ++g)
+            {
+                if( (*g).second > -1 ) return;
+            }
+
+            loc->second.startDeccel = CB_accel[0].first;
+            loc->second.lastAccels.clear();
+
+            loc->second.oldSpeed = CB_speed[0].second;
+            loc->second.lastSpeeds.clear();
+        }
+        else return;
+    }
+
+    // looking for the start of stopping delay
+    // NOTE: stopping delay might be zero
+    double startStopping = veh.startStopping;
+    if(startDeccel != -1 && startStopping == -1 && !crossed)
+    {
+        boost::circular_buffer<std::pair<double,double>> CB_speed = veh.lastSpeeds;
+        double speed = TraCI->vehicleGetSpeed(vID);
+        loc->second.lastSpeeds.push_back( std::make_pair(simTime().dbl(), speed) );
+
+        if(CB_speed.full())
+        {
+            for(boost::circular_buffer<std::pair<double,double>>::iterator g = CB_speed.begin(); g != CB_speed.end(); ++g)
+            {
+                if( (*g).second > 0.7 ) return;
+            }
+
+            loc->second.startStopping = CB_speed[0].first;
+            loc->second.lastSpeeds.clear();
+        }
+    }
+
+    // looking for the start of accel delay
+    double startAccel = veh.startAccel;
+    if(startDeccel != -1 && startAccel == -1 && crossed)
+    {
+        boost::circular_buffer<std::pair<double,double>> CB_accel = veh.lastAccels;
+        double accel = TraCI->vehicleGetCurrentAccel(vID);
+        loc->second.lastAccels.push_back( std::make_pair(simTime().dbl(), accel) );
+
+        if(CB_accel.full())
+        {
+            for(boost::circular_buffer<std::pair<double,double>>::iterator g = CB_accel.begin(); g != CB_accel.end(); ++g)
+            {
+                if( (*g).second < 1 ) return;
+            }
+
+            loc->second.startAccel = CB_accel[0].first;
+            loc->second.lastAccels.clear();
+        }
+        else return;
+    }
+
+    // looking for the end of delay
+    double endDelay = veh.endDelay;
+    double oldSpeed = veh.oldSpeed;
+    if(startDeccel != -1 && startAccel != -1 && endDelay == -1 && oldSpeed > 0 && crossed)
+    {
+        double speed = TraCI->vehicleGetSpeed(vID);
+
+        if(speed >= oldSpeed)
+            loc->second.endDelay = simTime().dbl();
+        else return;
+    }
 }
 
 
 void VehDelay::vehiclesDelayToFile()
 {
+    boost::filesystem::path filePath;
 
+    if( ev.isGUI() )
+    {
+        filePath = "results/gui/vehicleDelay.txt";
+    }
+    else
+    {
+        // get the current run number
+        int currentRun = ev.getConfigEx()->getActiveRunNumber();
+        std::ostringstream fileName;
+        fileName << currentRun << "_MACdata.txt";
+        filePath = "results/cmd/" + fileName.str();
+    }
+
+    FILE *filePtr = fopen (filePath.string().c_str(), "w");
+
+    // write header
+    fprintf (filePtr, "%-20s","vehicleName");
+    fprintf (filePtr, "%-15s","TLid");
+    fprintf (filePtr, "%-20s","lastLane");
+    fprintf (filePtr, "%-15s","entrance");
+    fprintf (filePtr, "%-15s","crossed?");
+    fprintf (filePtr, "%-15s","crossedT");
+    fprintf (filePtr, "%-15s","oldSpeed");
+    fprintf (filePtr, "%-15s","startDeccel");
+    fprintf (filePtr, "%-15s","startStopping");
+    fprintf (filePtr, "%-15s","startAccel");
+    fprintf (filePtr, "%-15s\n\n","endDelay");
+
+    // write body
+    for(std::map<std::string, delayEntry>::iterator y =  intersectionDelay.begin(); y != intersectionDelay.end(); ++y)
+    {
+        fprintf (filePtr, "%-20s", (*y).first.c_str());
+        fprintf (filePtr, "%-15s", (*y).second.TLid.c_str());
+        fprintf (filePtr, "%-20s", (*y).second.lastLane.c_str());
+        fprintf (filePtr, "%-15.2f", (*y).second.intersectionEntrance);
+        fprintf (filePtr, "%-15d", (*y).second.crossedIntersection);
+        fprintf (filePtr, "%-15.2f", (*y).second.crossedTime);
+        fprintf (filePtr, "%-15.2f", (*y).second.oldSpeed);
+        fprintf (filePtr, "%-15.2f", (*y).second.startDeccel);
+        fprintf (filePtr, "%-15.2f", (*y).second.startStopping);
+        fprintf (filePtr, "%-15.2f", (*y).second.startAccel);
+        fprintf (filePtr, "%-15.2f\n", (*y).second.endDelay);
+    }
+
+    fclose(filePtr);
 }
 
 
--- application/trafficLight/03_VehDelay.h
+++ application/trafficLight/03_VehDelay.h
@@ -29,7 +29,6 @@
 
 #include "02_LoopDetectors.h"
 
-
 namespace VENTOS {
 
 class delayEntry
@@ -37,19 +36,34 @@ class delayEntry
 public:
     std::string TLid;
     std::string lastLane;
+    double intersectionEntrance;
+    bool crossedIntersection;
+    double crossedTime;
+    double oldSpeed;
     double startDeccel;
     double startStopping;
     double startAccel;
     double endDelay;
+    boost::circular_buffer<std::pair<double,double>> lastSpeeds;
+    boost::circular_buffer<std::pair<double,double>> lastAccels;
 
-    delayEntry(std::string str1, std::string str2, double d1, double d2, double d3, double d4)
+    delayEntry(std::string str1, std::string str2, double d0, bool b1, double d1,
+            double d2, double d3, double d4, double d5, double d6,
+            boost::circular_buffer<std::pair<double,double>> CB_speed,
+            boost::circular_buffer<std::pair<double,double>> CB_accel)
     {
         this->TLid = str1;
         this->lastLane = str2;
-        this->startDeccel = d1;
-        this->startStopping = d2;
-        this->startAccel = d3;
-        this->endDelay = d4;
+        this->intersectionEntrance = d0;
+        this->crossedIntersection = b1;
+        this->crossedTime = d1;
+        this->oldSpeed = d2;
+        this->startDeccel = d3;
+        this->startStopping = d4;
+        this->startAccel = d5;
+        this->endDelay = d6;
+        this->lastSpeeds = CB_speed;
+        this->lastAccels = CB_accel;
     }
 };
 
@@ -73,9 +87,10 @@ private:
 
 protected:
     bool measureVehDelay;
-    std::list<std::string> TLList;      // list of traffic-lights in the network
+
+    // class variables
     std::list<std::string> lanesList;   // list of all lanes in the network
-    std::map<std::string, delayEntry> delay;
+    std::map<std::string, delayEntry> intersectionDelay;
 };
 
 }
