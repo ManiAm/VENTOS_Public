306843dccf2fd61f0d7d2ce9186914c869394b99
Mani Amoozadeh <maniam@ucdavis.edu>
2016-02-22 17:30:05 -0800

    adding color system conversion (RGB, HSL, HSV)
    Former-commit-id: fba21b537f5cffd0ac88008a4ba5854822cd25e3
    Former-commit-id: 8adcf5e4027dd043052c8b0b0173fa0c4077059a

--- application/global/Plotter.cc
+++ application/global/Plotter.cc
@@ -26,6 +26,7 @@
 //
 
 #include <Plotter.h>
+#include <algorithm>
 
 namespace VENTOS {
 
@@ -90,7 +91,7 @@ void Plotter::getVersion()
     // now parsing lineversion (gnuplot 5.0 patchlevel 1)
     sscanf(lineversion, "gnuplot %lf", &vers);
 
-    int pos= -1;
+    int pos = -1;
     char* restvers = NULL;
     if (sscanf(lineversion, "gnuplot %d.%d %n", &majvers, &minvers, &pos) >= 2)
     {
@@ -119,6 +120,241 @@ void Plotter::finish()
 void Plotter::handleMessage(cMessage *msg)
 {
 
+}
+
+
+double hue2rgb(double p, double q, double t)
+{
+    if(t < 0) t += 1;
+    if(t > 1) t -= 1;
+    if(t < 1/(double)6) return p + (q - p) * 6 * t;
+    if(t < 1/(double)2) return q;
+    if(t < 2/(double)3) return p + (q - p) * (2/(double)3 - t) * 6;
+
+    return p;
+}
+
+
+// more info here: http://colorizer.org/
+RGB Plotter::hslToRgb(double hue, double saturation, double lightness)
+{
+    hue /= 360, saturation /= 100, lightness /= 100;  // normalize value between [0,1]
+    double red, green, blue;
+
+    if(saturation == 0)
+    {
+        red = green = blue = lightness; // achromatic
+    }
+    else
+    {
+        double q = lightness < 0.5 ? lightness * (1 + saturation) : lightness + saturation - lightness * saturation;
+        double p = 2 * lightness - q;
+        red = hue2rgb(p, q, hue + 1/(double)3);
+        green = hue2rgb(p, q, hue);
+        blue = hue2rgb(p, q, hue - 1/(double)3);
+    }
+
+    RGB result;
+    result.red = red * 255;
+    result.green = green * 255;
+    result.blue = blue * 255;
+
+    return result;
+}
+
+
+// more info here: http://colorizer.org/
+HSL Plotter::rgbToHsl(double red, double green, double blue)
+{
+    red /= 255, green /= 255, blue /= 255; // normalize value between [0,1]
+    double max = std::max(red, std::max(green, blue));
+    double min = std::min(red, std::min(green, blue));
+    double h, s, l = (max + min) / 2;
+
+    if(max == min)
+    {
+        h = s = 0; // achromatic
+    }
+    else
+    {
+        double d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        if(max == red)
+            h = (green - blue) / d + (green < blue ? 6 : 0);
+        else if(max == green)
+            h = (blue - red) / d + 2;
+        else if(max == blue)
+            h = (red - green) / d + 4;
+
+        h /= 6;
+    }
+
+    HSL result;
+    result.hue = h * 360;
+    result.saturation = s * 100;
+    result.lightness = l * 100;
+
+    return result;
+}
+
+
+HSV Plotter::rgb2hsv(double red, double green, double blue)
+{
+    double min = std::min(red, std::min(green, blue));
+    double max = std::max(red, std::max(green, blue));
+    double delta = max - min;
+
+    HSV result;
+    result.value = 100 * (max / 255);
+
+    if (delta < 0.00001)
+    {
+        result.saturation = 0;
+        result.hue = 0; // undefined, maybe nan?
+        return result;
+    }
+
+    // NOTE: if Max is == 0, this divide would cause a crash
+    if( max > 0.0 )
+    {
+        result.saturation = 100 * (delta / max);
+    }
+    else
+    {
+        // if max is 0, then r = g = b = 0
+        // s = 0, v is undefined
+        result.saturation = 0.0;
+        result.hue = -1;       // its now undefined
+        return result;
+    }
+
+    if( red >= max )                           // > is bogus, just keeps compiler happy
+        result.hue = ( green - blue ) / delta;        // between yellow & magenta
+    else if( green >= max )
+        result.hue = 2.0 + ( blue - red ) / delta;  // between cyan & yellow
+    else
+        result.hue = 4.0 + ( red - green ) / delta;  // between magenta & cyan
+
+    result.hue *= 60.0;                              // degrees
+
+    if( result.hue < 0.0 )
+        result.hue += 360.0;
+
+    return result;
+}
+
+
+RGB Plotter::hsv2rgb(double hue, double saturation, double value)
+{
+    saturation /= 100, value /= 100;  // normalize value between [0,1]
+    double hh, p, q, t, ff;
+    RGB result;
+
+    // < is bogus, just shuts up warnings
+    if(saturation <= 0.0)
+    {
+        result.red = value;
+        result.green = value;
+        result.blue = value;
+
+        return result;
+    }
+
+    hh = hue;
+    if(hh >= 360.0) hh = 0.0;
+    hh /= 60.0;
+    long i = (long)hh;
+    ff = hh - i;
+    p = value * (1.0 - saturation);
+    q = value * (1.0 - (saturation * ff));
+    t = value * (1.0 - (saturation * (1.0 - ff)));
+
+    switch(i)
+    {
+    case 0:
+        result.red = value * 255;
+        result.green = t * 255;
+        result.blue = p * 255;
+        break;
+    case 1:
+        result.red = q * 255;
+        result.green = value * 255;
+        result.blue = p * 255;
+        break;
+    case 2:
+        result.red = p * 255;
+        result.green = value * 255;
+        result.blue = t * 255;
+        break;
+
+    case 3:
+        result.red = p * 255;
+        result.green = q * 255;
+        result.blue = value * 255;
+        break;
+    case 4:
+        result.red = t * 255;
+        result.green = p * 255;
+        result.blue = value * 255;
+        break;
+    case 5:
+    default:
+        result.red = value * 255;
+        result.green = p * 255;
+        result.blue = q * 255;
+        break;
+    }
+
+    return result;
+}
+
+
+HSV Plotter::getUniqueHSVColor()
+{
+    if(uniqueColors.empty())
+        error("No more colors exist!");
+
+    HSV color = uniqueColors.back();
+    uniqueColors.pop_back();
+    return color;
+}
+
+
+// change saturation from minSaturation to 100
+std::vector<double> Plotter::generateColorShades(unsigned int num_shades)
+{
+    const double minSaturation = 40.0;
+
+    if(num_shades <= 0)
+        error("num_shades is not right!");
+
+    std::vector<double> v;
+
+    if(num_shades == 1)
+    {
+        v.push_back(100);
+        return v;
+    }
+
+    double offset = (100-minSaturation) / (double)(num_shades-1);
+    double count = minSaturation;
+    for(unsigned int i = 1; i <= num_shades; ++i)
+    {
+        v.push_back(count);
+        count = count + offset;
+    }
+
+    // reverse the vector
+    std::reverse(v.begin(), v.end());
+
+    return v;
+}
+
+
+unsigned long Plotter::createRGB(int r, int g, int b)
+{
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
 
 }
--- application/global/Plotter.h
+++ application/global/Plotter.h
@@ -33,6 +33,21 @@
 
 namespace VENTOS {
 
+struct RGB
+{
+    double red, green, blue;
+};
+
+struct HSV
+{
+    double hue, saturation, value;
+};
+
+struct HSL
+{
+    double hue, saturation, lightness;
+};
+
 class Plotter : public BaseApplLayer
 {
   public:
@@ -40,6 +55,16 @@ class Plotter : public BaseApplLayer
       virtual void finish();
       virtual void initialize(int);
       virtual void handleMessage(cMessage *);
+
+      HSL rgbToHsl(double, double, double);
+      RGB hslToRgb(double, double, double);
+
+      HSV rgb2hsv(double, double, double);
+      RGB hsv2rgb(double, double, double);
+
+      HSV getUniqueHSVColor();
+      std::vector<double> generateColorShades(unsigned int);
+      unsigned long createRGB(int r, int g, int b);
 
   private:
       void getVersion();
@@ -54,6 +79,12 @@ class Plotter : public BaseApplLayer
   private:
       TraCI_Commands *TraCI;
       bool on;
+      // colors in HSV model
+      std::vector<HSV> uniqueColors {
+          {120, 100, 100},  // green
+          {0, 100, 100},    // red
+          {240, 100, 100}   // blue
+      };
 };
 
 }
