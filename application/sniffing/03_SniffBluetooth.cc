38b64356e928d4879aaff3f239d10da97202a6fd
Mani Amoozadeh <maniam@ucdavis.edu>
2016-03-03 15:39:02 -0800

    adding comments for BLE  parameters
    Former-commit-id: 124b95d62bbf36e720a68388ff4e63870fa54588
    Former-commit-id: c29cc90267d14ecb081b407b37dd182fd939af43

--- application/sniffing/03_SniffBluetooth.cc
+++ application/sniffing/03_SniffBluetooth.cc
@@ -236,7 +236,7 @@ void SniffBluetooth::print_dev_info(struct hci_dev_info *di)
             di->features[4], di->features[5],
             di->features[6], di->features[7]);
 
-    printf("\tLE support: ");
+    printf("\tBLE support: ");
     std::string featuresStr = lmp_featurestostr(di->features, "\t\t", 63);
     if(featuresStr.find("LE support") != std::string::npos)
         printf("Yes \n");
