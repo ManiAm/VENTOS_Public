dd795f98ba52596ed1984bce3306731319e151f5
Mani Amoozadeh <maniam@ucdavis.edu>
2016-05-14 18:21:23 -0800

    add copyright information

--- src/logging/vlogConst.h
+++ src/logging/vlogConst.h
@@ -1,3 +1,29 @@
+/****************************************************************************/
+/// @file    vLogConsts.h
+/// @author  Mani Amoozadeh <maniam@ucdavis.edu>
+/// @author  second author name
+/// @date    May 2016
+///
+/****************************************************************************/
+// VENTOS, Vehicular Network Open Simulator; see http:?
+// Copyright (C) 2013-2015
+/****************************************************************************/
+//
+// This file is part of VENTOS.
+// VENTOS is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
 
 namespace VENTOS {
 
@@ -17,7 +43,7 @@ enum logWindowCMD
 #define   ALL_LOG_VAL       0b11111111
 #define   NO_LOG_VAL        0b00000000
 
-// logs redirected to the standard output stream
+// logs are redirected to the standard output stream (std::cout)
 
 #define WARNING_LOG \
         std::lock_guard<std::mutex>{vlog::lock_log}, vlog::WARNING()
@@ -37,7 +63,7 @@ enum logWindowCMD
 #define FLUSH_LOG \
         std::lock_guard<std::mutex>{vlog::lock_log}, vlog::FLUSH()
 
-// logs redirected to the log window -- category andsubcategory
+// logs are redirected to the 'logWindow'. You can control it using category and subcategory
 
 #define WARNING_LOG_C(category, subcategory) \
         std::lock_guard<std::mutex>{vlog::lock_log}, vlog::WARNING(category, subcategory)
