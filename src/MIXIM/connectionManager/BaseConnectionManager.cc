3fde0c0247ee2fc027417058a355f13211194665
Mani Amoozadeh <maniam@ucdavis.edu>
2017-01-21 21:54:04 -0800

    now we can show/hide the max interference dist for each node

--- src/MIXIM/connectionManager/BaseConnectionManager.cc
+++ src/MIXIM/connectionManager/BaseConnectionManager.cc
@@ -23,7 +23,8 @@ static double dist(double coord1, double coord2, double size) {
     if (difference == 0)
         // NOTE: event if size is zero
         return 0;
-    else {
+    else
+    {
         assert(size != 0);
         double dist = FWMath::modulo(difference, size);
         return std::min(dist, size - dist);
@@ -43,28 +44,16 @@ void BaseConnectionManager::initialize(int stage)
 
     if (stage == 0)
     {
-        if(hasPar("coreDebug"))
-            coreDebug = par("coreDebug").boolValue();
-        else
-            coreDebug = false;
+        coreDebug = hasPar("coreDebug") ? par("coreDebug").boolValue() : false;
+        sendDirect = hasPar("sendDirect") ? par("sendDirect").boolValue() : false;
 
-        drawMIR = hasPar("drawMaxIntfDist")
-						        ? par("drawMaxIntfDist").boolValue() : false;
+        ccEV << "initializing BaseConnectionManager \n";
 
-        ccEV <<"initializing BaseConnectionManager\n";
-
-        BaseWorldUtility* world = FindModule<BaseWorldUtility*>
-        ::findGlobalModule();
-
+        BaseWorldUtility* world = FindModule<BaseWorldUtility*>::findGlobalModule();
         assert(world != 0);
 
         playgroundSize = world->getPgs();
         useTorus = world->useTorus();
-
-        if(hasPar("sendDirect"))
-            sendDirect = par("sendDirect").boolValue();
-        else
-            sendDirect = false;
 
         maxInterferenceDistance = calcInterfDist();
         maxDistSquared = maxInterferenceDistance * maxInterferenceDistance;
@@ -85,42 +74,46 @@ void BaseConnectionManager::initialize(int stage)
             gridDim.x = 1;
             gridDim.y = 1;
             gridDim.z = 1;
-        } else {
+        }
+        else
+        {
             gridDim.x = std::max(1, gridDim.x);
             gridDim.y = std::max(1, gridDim.y);
             gridDim.z = std::max(1, gridDim.z);
         }
 
-        //step 2 - initialize the matrix which represents our grid
+        // step 2 - initialize the matrix which represents our grid
         NicEntries entries;
         RowVector row;
         NicMatrix matrix;
 
-        for (int i = 0; i < gridDim.z; ++i) {
-            row.push_back(entries);			//copy empty NicEntries to RowVector
-        }
-        for (int i = 0; i < gridDim.y; ++i) {//fill the ColVector with copies of
-            matrix.push_back(row);			 //the RowVector.
-        }
-        for (int i = 0; i < gridDim.x; ++i) {	//fill the grid with copies of
-            nicGrid.push_back(matrix);			//the matrix.
-        }
-        ccEV << " using " << gridDim.x << "x" <<
-                gridDim.y << "x" <<
-                gridDim.z << " grid" << std::endl;
+        // copy empty NicEntries to RowVector
+        for (int i = 0; i < gridDim.z; ++i)
+            row.push_back(entries);
 
-        //step 3 -	calculate the factor which maps the coordinate of a node
-        //			to the grid cell
-        //if we use a 1x1 grid every coordinate is mapped to (0,0, 0)
+        // fill the ColVector with copies of the RowVector
+        for (int i = 0; i < gridDim.y; ++i)
+            matrix.push_back(row);
+
+        // fill the grid with copies of the matrix
+        for (int i = 0; i < gridDim.x; ++i)
+            nicGrid.push_back(matrix);
+
+        ccEV << " using " << gridDim.x << "x" << gridDim.y << "x" << gridDim.z << " grid" << std::endl;
+
+        //step 3- calculate the factor which maps the coordinate of a node to the grid cell
+        // if we use a 1x1 grid every coordinate is mapped to (0,0, 0)
         findDistance = Coord(std::max(playgroundSize->x, maxInterferenceDistance),
                 std::max(playgroundSize->y, maxInterferenceDistance),
                 std::max(playgroundSize->z, maxInterferenceDistance));
-        //otherwise we divide the playground into cells of size of the maximum
-        //interference distance
+
+        // otherwise we divide the playground into cells of size of the maximum interference distance
         if (gridDim.x != 1)
             findDistance.x = playgroundSize->x / gridDim.x;
+
         if (gridDim.y != 1)
             findDistance.y = playgroundSize->y / gridDim.y;
+
         if (gridDim.z != 1)
             findDistance.z = playgroundSize->z / gridDim.z;
 
@@ -131,16 +124,17 @@ void BaseConnectionManager::initialize(int stage)
         //This also assures that findDistance is never zero.
         findDistance += Coord(EPSILON, EPSILON, EPSILON);
 
-        //findDistance (equals cell size) has to be greater or equal
+        // findDistance (equals cell size) has to be greater or equal
         //maxInt-distance
         assert(findDistance.x >= maxInterferenceDistance);
         assert(findDistance.y >= maxInterferenceDistance);
         assert(findDistance.z >= maxInterferenceDistance);
 
-        //playGroundSize has to be part of the playGround
+        // playGroundSize has to be part of the playGround
         assert(GridCoord(*playgroundSize, findDistance).x == gridDim.x - 1);
         assert(GridCoord(*playgroundSize, findDistance).y == gridDim.y - 1);
         assert(GridCoord(*playgroundSize, findDistance).z == gridDim.z - 1);
+
         ccEV << "findDistance is " << findDistance.info() << std::endl;
     }
     else if (stage == 1)
@@ -149,15 +143,12 @@ void BaseConnectionManager::initialize(int stage)
     }
 }
 
-BaseConnectionManager::GridCoord BaseConnectionManager
-::getCellForCoordinate(const Coord& c)
+BaseConnectionManager::GridCoord BaseConnectionManager::getCellForCoordinate(const Coord& c)
 {
     return GridCoord(c, findDistance);
 }
 
-void BaseConnectionManager::updateConnections(int nicID,
-        const Coord* oldPos,
-        const Coord* newPos)
+void BaseConnectionManager::updateConnections(int nicID, const Coord* oldPos, const Coord* newPos)
 {
     GridCoord oldCell = getCellForCoordinate(*oldPos);
     GridCoord newCell = getCellForCoordinate(*newPos);
@@ -165,8 +156,7 @@ void BaseConnectionManager::updateConnections(int nicID,
     checkGrid(oldCell, newCell, nicID );
 }
 
-BaseConnectionManager::NicEntries& BaseConnectionManager
-::getCellEntries(BaseConnectionManager::GridCoord& cell)
+BaseConnectionManager::NicEntries& BaseConnectionManager::getCellEntries(BaseConnectionManager::GridCoord& cell)
 {
     return nicGrid[cell.x][cell.y][cell.z];
 }
@@ -177,91 +167,92 @@ void BaseConnectionManager::registerNicExt(int nicID)
 
     GridCoord cell = getCellForCoordinate(nicEntry->pos);
 
-    ccEV <<" registering (ext) nic at loc " << cell.info() << std::endl;
+    ccEV << " registering (ext) nic at loc " << cell.info() << std::endl;
 
     // add to matrix
     NicEntries& cellEntries = getCellEntries(cell);
     cellEntries[nicID] = nicEntry;
 }
 
-void BaseConnectionManager::checkGrid(BaseConnectionManager::GridCoord& oldCell,
-        BaseConnectionManager::GridCoord& newCell,
-        int id)
-
+void BaseConnectionManager::checkGrid(BaseConnectionManager::GridCoord& oldCell, BaseConnectionManager::GridCoord& newCell, int id)
 {
-
     // structure to find union of grid squares
     CoordSet gridUnion(74);
 
     // find nic at old position
-    NicEntries&             oldCellEntries = getCellEntries(oldCell);
-    NicEntries::iterator    it  = oldCellEntries.find(id);
+    NicEntries& oldCellEntries = getCellEntries(oldCell);
+    NicEntries::iterator it  = oldCellEntries.find(id);
     NicEntries::mapped_type nic = it->second;
 
-
     // move nic to a new position in matrix
-    if(oldCell != newCell) {
+    if(oldCell != newCell)
+    {
         oldCellEntries.erase(it);
         getCellEntries(newCell)[id] = nic;
     }
 
-    if((gridDim.x == 1) && (gridDim.y == 1) && (gridDim.z == 1)) {
+    if((gridDim.x == 1) && (gridDim.y == 1) && (gridDim.z == 1))
+    {
         gridUnion.add(oldCell);
-    } else {
-        //add grid around oldPos
+    }
+    else
+    {
+        // add grid around oldPos
         fillUnionWithNeighbors(gridUnion, oldCell);
 
-
-        if(oldCell != newCell) {
-            //add grid around newPos
+        // add grid around newPos
+        if(oldCell != newCell)
             fillUnionWithNeighbors(gridUnion, newCell);
-        }
     }
 
     GridCoord* c = gridUnion.next();
-    while(c != 0) {
+    while(c != 0)
+    {
         ccEV << "Update cons in [" << c->info() << "]" << std::endl;
         updateNicConnections(getCellEntries(*c), nic);
         c = gridUnion.next();
     }
 }
 
-int BaseConnectionManager::wrapIfTorus(int value, int max) {
-    if(value < 0) {
-        if(useTorus) {
+int BaseConnectionManager::wrapIfTorus(int value, int max)
+{
+    if(value < 0)
+    {
+        if(useTorus)
             return max + value;
-        } else {
+        else
             return -1;
-        }
-    } else if(value >= max) {
-        if(useTorus) {
-            return value - max;
-        } else {
-            return -1;
-        }
-    } else {
-        return value;
     }
+    else if(value >= max)
+    {
+        if(useTorus)
+            return value - max;
+        else
+            return -1;
+    }
+    else
+        return value;
 }
 
-void BaseConnectionManager::fillUnionWithNeighbors(CoordSet& gridUnion,
-        GridCoord cell)
+void BaseConnectionManager::fillUnionWithNeighbors(CoordSet& gridUnion, GridCoord cell)
 {
-    for(int iz = (int)cell.z - 1; iz <= (int)cell.z + 1; iz++) {
+    for(int iz = (int)cell.z - 1; iz <= (int)cell.z + 1; iz++)
+    {
         int cz = wrapIfTorus(iz, gridDim.z);
-        if(cz == -1) {
+        if(cz == -1)
             continue;
-        }
-        for(int ix = (int)cell.x - 1; ix <= (int)cell.x + 1; ix++) {
+
+        for(int ix = (int)cell.x - 1; ix <= (int)cell.x + 1; ix++)
+        {
             int cx = wrapIfTorus(ix, gridDim.x);
-            if(cx == -1) {
+            if(cx == -1)
                 continue;
-            }
-            for(int iy = (int)cell.y - 1; iy <= (int)cell.y + 1; iy++) {
+
+            for(int iy = (int)cell.y - 1; iy <= (int)cell.y + 1; iy++)
+            {
                 int cy = wrapIfTorus(iy, gridDim.y);
-                if(cy != -1) {
+                if(cy != -1)
                     gridUnion.add(GridCoord(cx, cy, cz));
-                }
             }
         }
     }
@@ -271,19 +262,20 @@ bool BaseConnectionManager::isInRange(BaseConnectionManager::NicEntries::mapped_
 {
     double dDistance = 0.0;
 
-    if(useTorus) {
+    if(useTorus)
         dDistance = sqrTorusDist(pFromNic->pos, pToNic->pos, *playgroundSize);
-    } else {
+    else
         dDistance = pFromNic->pos.sqrdist(pToNic->pos);
-    }
+
     return (dDistance <= maxDistSquared);
 }
 
-void BaseConnectionManager::updateNicConnections(NicEntries& nmap, BaseConnectionManager::NicEntries::mapped_type   nic)
+void BaseConnectionManager::updateNicConnections(NicEntries& nmap, BaseConnectionManager::NicEntries::mapped_type nic)
 {
     int id = nic->nicId;
 
-    for(NicEntries::iterator i = nmap.begin(); i != nmap.end(); ++i) {
+    for(NicEntries::iterator i = nmap.begin(); i != nmap.end(); ++i)
+    {
         NicEntries::mapped_type nic_i = i->second;
 
         // no recursive connections
@@ -292,28 +284,26 @@ void BaseConnectionManager::updateNicConnections(NicEntries& nmap, BaseConnectio
         bool inRange   = isInRange(nic, nic_i);
         bool connected = nic->isConnected(nic_i);
 
-        if ( inRange && !connected ) {
+        if ( inRange && !connected )
+        {
             // nodes within communication range: connect
             // nodes within communication range && not yet connected
-            ccEV << "nic #" << id << " and #" << nic_i->nicId
-                    << " are in range" << std::endl;
+            ccEV << "nic #" << id << " and #" << nic_i->nicId << " are in range" << std::endl;
             nic->connectTo( nic_i );
             nic_i->connectTo( nic );
         }
-        else if ( !inRange && connected ) {
+        else if ( !inRange && connected )
+        {
             // out of range: disconnect
             // out of range, and still connected
-            ccEV << "nic #" << id << " and #" << nic_i->nicId
-                    << " are NOT in range" << std::endl;
+            ccEV << "nic #" << id << " and #" << nic_i->nicId << " are NOT in range" << std::endl;
             nic->disconnectFrom( nic_i );
             nic_i->disconnectFrom( nic );
         }
     }
 }
 
-bool BaseConnectionManager::registerNic(cModule* nic,
-        ChannelAccess* chAccess,
-        const Coord* nicPos)
+bool BaseConnectionManager::registerNic(cModule* nic, ChannelAccess* chAccess, const Coord* nicPos)
 {
     assert(nic != 0);
 
@@ -342,9 +332,8 @@ bool BaseConnectionManager::registerNic(cModule* nic,
 
     updateConnections(nicID, nicPos, nicPos);
 
-    if(drawMIR) {
+    if(nic->par("drawMaxIntfDist"))
         nic->getParentModule()->getDisplayString().setTagArg("r", 0, maxInterferenceDistance);
-    }
 
     return sendDirect;
 }
@@ -365,18 +354,19 @@ bool BaseConnectionManager::unregisterNic(cModule* nicModule)
     // get all affected grid squares
     CoordSet gridUnion(74);
     GridCoord cell = getCellForCoordinate(nicEntry->pos);
-    if((gridDim.x == 1) && (gridDim.y == 1) && (gridDim.z == 1)) {
+    if((gridDim.x == 1) && (gridDim.y == 1) && (gridDim.z == 1))
         gridUnion.add(cell);
-    } else {
+    else
         fillUnionWithNeighbors(gridUnion, cell);
-    }
 
     // disconnect from all NICs in these grid squares
     GridCoord* c = gridUnion.next();
-    while(c != 0) {
+    while(c != 0)
+    {
         ccEV << "Update cons in [" << c->info() << "]" << std::endl;
         NicEntries& nmap = getCellEntries(*c);
-        for(NicEntries::iterator i = nmap.begin(); i != nmap.end(); ++i) {
+        for(NicEntries::iterator i = nmap.begin(); i != nmap.end(); ++i)
+        {
             NicEntries::mapped_type other = i->second;
             if (other == nicEntry) continue;
             if (!other->isConnected(nicEntry)) continue;
@@ -419,8 +409,7 @@ const NicEntry::GateList& BaseConnectionManager::getGateList(int nicID) const
     return ItNic->second->getGateList();
 }
 
-const omnetpp::cGate* BaseConnectionManager::getOutGateTo(const NicEntry* nic,
-        const NicEntry* targetNic) const
+const omnetpp::cGate* BaseConnectionManager::getOutGateTo(const NicEntry* nic, const NicEntry* targetNic) const
 {
     NicEntries::const_iterator ItNic = nics.find(nic->nicId);
     if (ItNic == nics.end())
@@ -431,7 +420,6 @@ const omnetpp::cGate* BaseConnectionManager::getOutGateTo(const NicEntry* nic,
 
 BaseConnectionManager::~BaseConnectionManager()
 {
-    for (NicEntries::iterator ne = nics.begin(); ne != nics.end(); ne++) {
+    for (NicEntries::iterator ne = nics.begin(); ne != nics.end(); ne++)
         delete ne->second;
-    }
 }
--- src/MIXIM/connectionManager/ConnectionManager.cc
+++ src/MIXIM/connectionManager/ConnectionManager.cc
@@ -3,37 +3,30 @@
 #include "ConnectionManager.h"
 #include "BaseWorldUtility.h"
 
-#ifndef ccEV
-#define ccEV EV << getName() << ": "
-#endif
-
 Define_Module(ConnectionManager);
 
 double ConnectionManager::calcInterfDist()
 {
-	double interfDistance;
-
-	//the minimum carrier frequency for this cell
+	// the minimum carrier frequency for this cell
 	double carrierFrequency = par("carrierFrequency").doubleValue();
-	//maximum transmission power possible
-	double pMax             = par("pMax").doubleValue();
-	if (pMax <=0) {
-	    throw omnetpp::cRuntimeError("Max transmission power is <=0!");
-	}
-	//minimum signal attenuation threshold
-	double sat              = par("sat").doubleValue();
-	//minimum path loss coefficient
-	double alpha            = par("alpha").doubleValue();
 
-	double waveLength     = (BaseWorldUtility::speedOfLight()/carrierFrequency);
-	//minimum power level to be able to physically receive a signal
+	// maximum transmission power possible
+	double pMax = par("pMax").doubleValue();
+	if (pMax <= 0)
+	    throw omnetpp::cRuntimeError("Max transmission power is <= 0!");
+
+	// minimum signal attenuation threshold
+	double sat = par("sat").doubleValue();
+
+	// minimum path loss coefficient
+	double alpha = par("alpha").doubleValue();
+
+	double waveLength = (BaseWorldUtility::speedOfLight()/carrierFrequency);
+
+	// minimum power level to be able to physically receive a signal
 	double minReceivePower = pow(10.0, sat/10.0);
 
-	interfDistance = pow(waveLength * waveLength * pMax
-					       / (16.0*M_PI*M_PI*minReceivePower),
-					     1.0 / alpha);
-
-	ccEV << "max interference distance:" << interfDistance << std::endl;
+	double interfDistance = pow(waveLength * waveLength * pMax / (16.0*M_PI*M_PI*minReceivePower), 1.0 / alpha);
 
 	return interfDistance;
 }
