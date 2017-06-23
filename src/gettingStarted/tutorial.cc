006ff855964a50b044370a4a47d3faa7d76e3ea4
Calvin Hu <calvinhu9@gmail.com>
2017-06-23 15:49:21 -0700

    learning TraCI data logging (almost done)

--- src/gettingStarted/tutorial.cc
+++ src/gettingStarted/tutorial.cc
@@ -96,47 +96,58 @@ namespace VENTOS {
 
   void tutorial::initialize_withTraCI()
   {
-    int depart = 0;
-    const int interval = 1000;
-    for(int i=1; i<=100; i++)
-    {
-      char vehicleName[90];
-      sprintf(vehicleName, "veh_set1_%d", i);
-      TraCI->vehicleAdd(vehicleName, "passenger", "route0", depart, 0/*pos*/, 0 /*speed*/, 0 /*lane*/);
-      depart += interval;
-    }
+    /* Dynamically adding 5 vehicles in the beginning of the simulation */
+    // int depart = 0;
+    // const int interval = 1000;
+    // for(int i=1; i<=5; i++)
+    // {
+    //   char vehicleName[90];
+    //   sprintf(vehicleName, "veh_set1_%d", i);
+    //   TraCI->vehicleAdd(vehicleName, "passenger", "route0", depart, 0/*pos*/, 0 /*speed*/, 0 /*lane*/);
+    //   depart += interval;
+    // }
+
+    /* Forcing a rear-end collision (ForceCrash scenario)*/
+    TraCI->vehicleSetLaneChangeMode("veh0", 0);
+    TraCI->vehicleSetLaneChangeMode("veh1", 0);
   }
 
   void tutorial::executeEachTimestep()
   {
-    static int departedVehs = 0;
-    // get number of departed vehicles in the current time step
-    departedVehs += TraCI->simulationGetDepartedVehiclesCount();
-    static int arrivedVehs = 0;
-    // get the number of arrived vehicles in the current time step
-    arrivedVehs += TraCI->simulationGetArrivedNumber();
-    std::cout << "\ntime step: " << omnetpp::simTime().dbl();
-    std::cout << ", departed vehs: " << departedVehs;
-    std::cout << ", arrived vehs: " << arrivedVehs;
-    std::cout << "\n" << std::flush;
+    /* Dynamically adding 5 vehicles in the middle of the simulation */
+    // static int departedVehs = 0;
+    // // get number of departed vehicles in the current time step
+    // departedVehs += TraCI->simulationGetDepartedVehiclesCount();
+    // static int arrivedVehs = 0;
+    // // get the number of arrived vehicles in the current time step
+    // arrivedVehs += TraCI->simulationGetArrivedNumber();
+    // std::cout << "\ntime step: " << omnetpp::simTime().dbl();
+    // std::cout << ", departed vehs: " << departedVehs;
+    // std::cout << ", arrived vehs: " << arrivedVehs;
+    // std::cout << "\n" << std::flush;
+    // // static bool wasExecuted = false;
+    // if(/*!wasExecuted && */departedVehs == 10)
+    // {
+    //   int depart = omnetpp::simTime().dbl() * 1000;
+    //   const int interval = 1000;
+    //   for(int i=1; i<=5; i++)
+    //   {
+    //     char vehicleName[90];
+    //     sprintf(vehicleName, "veh_set2_%d", i);
+    //     TraCI->vehicleAdd(vehicleName, "passenger", "route0", depart, 0 /*pos*/, 0 /*speed*/, 1 /*lane*/);
+    //     // change color to red
+    //     RGB newColor = Color::colorNameToRGB("red");
+    //     TraCI->vehicleSetColor(vehicleName, newColor);
+    //     depart += interval;
+    //   }
+    //   wasExecuted = true;
+    // }
 
-    // static bool wasExecuted = false;
-    if(/*!wasExecuted && */departedVehs == 10)
+    /* Forcing a rear-end collision (ForceCrash scenario)*/
+    if(omnetpp::simTime().dbl() == 20)
     {
-      int depart = omnetpp::simTime().dbl() * 1000;
-      const int interval = 1000;
-      for(int i=1; i<=5; i++)
-      {
-        char vehicleName[90];
-        sprintf(vehicleName, "veh_set2_%d", i);
-        TraCI->vehicleAdd(vehicleName, "passenger", "route0", depart, 0 /*pos*/, 0 /*speed*/, 1 /*lane*/);
-        // change color to red
-        RGB newColor = Color::colorNameToRGB("red");
-        TraCI->vehicleSetColor(vehicleName, newColor);
-        depart += interval;
-      }
-      // wasExecuted = true;
+      // TraCI->vehicleSetSpeedMode("veh0", 27);
+      TraCI->vehicleSetSpeed("veh0", 0);
     }
   }
-
 }
